6324aa4a184553478c2e80bcea1475357775d97e
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2019-09-29 22:42:15 +0300

    Setup files, nothing is ready yet...

---
+++ README.md
@@ -0,0 +1,42 @@
+[![PyPI version](https://badge.fury.io/py/django-vb-console.svg)](https://badge.fury.io/py/django-vb-console)
+
+
+# django-vb-console
+
+@wip
+
+## Installation and Usage
+
+@wip
+
+---
+
+## License
+
+This project is licensed under MIT
+
+---
+
+## Contributer(s)
+
+* [Uğur "vigo" Özyılmazel](https://github.com/vigo) - Creator, maintainer
+
+---
+
+## Contribute
+
+All PR’s are welcome!
+
+1. `fork` (https://github.com/vbyazilim/django-vb-console/fork)
+1. Create your `branch` (`git checkout -b my-features`)
+1. `commit` yours (`git commit -am 'added killer options'`)
+1. `push` your `branch` (`git push origin my-features`)
+1. Than create a new **Pull Request**!
+
+---
+
+## Change Log
+
+**2019-09-29**
+
+- Init repo!
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+
+from setuptools import setup
+
+CURRENT_WORKING_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(CURRENT_WORKING_DIRECTORY, 'README.md')) as fp:
+    README = fp.read()
+
+setup(
+    name='django-vb-console',
+    version='0.1.0',
+    description='Log to console in test or development mode',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vbyazilim/django-vb-console',
+    author='vb YAZILIM',
+    author_email='hello@vbyazilim.com',
+    license='MIT',
+    python_requires='>=3.0',
+    packages=['vb_console'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+)
