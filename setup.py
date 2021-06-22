1bf3029543e37e09f7c291cbe1deddc42768bbea
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-06-22 10:53:24 +0300

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(CURRENT_WORKING_DIRECTORY, 'README.md')) as fp:
 
 setup(
     name='vb-console',
-    version='1.1.0',
+    version='1.1.1',
     description='Logger and object inspector for Python',
     long_description=README,
     long_description_content_type='text/markdown',
